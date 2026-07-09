080722016ce7016db3a415dbc8d3c07235c8a1af
YARN-DEV <beeowensby850@gmail.com>
2026-07-09 02:09:48 -0400

    Fix: force-dynamic on all pages to prevent Vercel prerender errors

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useEffect, useState } from 'react'
 import BottomNav from '@/components/BottomNav'
 import styles from './page.module.css'
